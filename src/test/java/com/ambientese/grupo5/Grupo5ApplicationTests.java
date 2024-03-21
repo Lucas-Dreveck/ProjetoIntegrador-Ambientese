4aa89b0bbb6ece33ee6dec6551ee90faec35db04
Lucas-Dreveck <lucasdrevc@outlook.com>
2024-03-21 20:52:39 -0300

    Feat: Create project structure for spring boot with some dependecies:  Spring Data JPA  Spring Web  Spring Boot DevTools  MySQL Driver

---
+++ src/test/java/com/ambientese/grupo5/Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambientese.grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
