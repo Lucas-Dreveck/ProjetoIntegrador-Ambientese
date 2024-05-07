e24d3ccdc934e1700042906352d2733c962b9bd4
Lucas-Dreveck <lucasdrevc@outlook.com>
2024-05-07 20:00:07 -0300

    Feat: create base layout with:  - Header;  - Menu;  - Favico;  - JSs;  - CSSs;  - Routes to pages;

---
+++ src/main/java/com/ambientese/grupo5/View/Pages.java
@@ -0,0 +1,33 @@
+package com.ambientese.grupo5.View;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class Pages {
+
+    @GetMapping("/empresas")
+    public String pageEmpresas(Model model) throws InterruptedException {
+        Thread.sleep(2000); // Simulated Lag
+        return "pages/cadastro-empresas";
+    }
+
+    @GetMapping("/funcionarios")
+    public String pageFuncionarios(Model model) throws InterruptedException {
+        Thread.sleep(2000); // Simulated Lag
+        return "pages/cadastro-funcionarios";
+    }
+
+    @GetMapping("/ranking")
+    public String pageRanking(Model model) throws InterruptedException {
+        Thread.sleep(2000); // Simulated Lag
+        return "pages/ranking";
+    }
+
+    @GetMapping("/avaliacao")
+    public String pageAvaliacao(Model model) throws InterruptedException {
+        Thread.sleep(2000); // Simulated Lag
+        return "pages/avaliacao";
+    }
+}
