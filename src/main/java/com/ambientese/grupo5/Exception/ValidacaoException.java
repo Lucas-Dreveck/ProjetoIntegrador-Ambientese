6c9bae37176be6ec22f1b12cd016e5094483bd7a
Luiz Gabriel <lgabrielpagliari@gmail.com>
2024-05-08 16:41:28 +0000

    Revert "Commit das alterações antes de fazer checkout para a branch develop"
    This reverts commit 0b2473fa650c272ecbc3d4cd46a758661347b530

--- src/main/java/com/ambientese/grupo5/Exception/ValidacaoException.java
+++
@@ -1,8 +0,0 @@
-    package com.Ambientese.Empresa.Exception;
-
-    public class ValidacaoException extends RuntimeException {
-
-        public ValidacaoException(String message) {
-            super(message);
-        }
-    }
--- src/main/java/com/ambientese/grupo5/Repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Ambientese.Empresa.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Ambientese.Empresa.Model.UsuarioModel;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-
-}
