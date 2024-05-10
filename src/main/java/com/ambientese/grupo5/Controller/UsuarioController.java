9bfe8d609c28e1fe3fe7f11751b840898bca77c8
Luiz Gabriel Pagliari Moreira <luiz.moreira@alunos.bpkedu.com.br>
2024-05-09 21:17:09 -0300

    ajustes usuariocontroller

--- src/main/java/com/ambientese/grupo5/Controller/UsuarioController.java
+++ src/main/java/com/ambientese/grupo5/Controller/UsuarioController.java
@@ -59,17 +59,12 @@ public class UsuarioController {
 
 
         if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
-            // Se o nome de usuário ou senha estiver vazio, retorna um erro de solicitação inválida
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nome de usuário e senha são obrigatórios!");
         }
 
-        // Lógica de autenticação simples para verificar se o nome de usuário e senha correspondem a um usuário específico
         if ("usuario_correto".equals(username) && "senha_correta".equals(password)) {
-            // Se o login for bem-sucedido, você pode retornar um token JWT ou apenas uma mensagem de sucesso
-            // Neste exemplo, estou retornando uma mensagem simples
             return ResponseEntity.ok("Login bem-sucedido!");
         } else {
-            // Se as credenciais estiverem incorretas, você pode retornar uma mensagem de erro
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha incorretos!");
         }
     }
