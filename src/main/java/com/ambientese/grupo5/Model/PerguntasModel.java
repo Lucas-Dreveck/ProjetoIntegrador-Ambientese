24d6a53e794ab6d0dd3acead8b171f0dda520eeb
Lucas-Dreveck <lucasdrevc@outlook.com>
2024-05-21 18:02:22 -0300

    fix: some columns with wrong format / identation

--- src/main/java/com/ambientese/grupo5/Model/PerguntasModel.java
+++ src/main/java/com/ambientese/grupo5/Model/PerguntasModel.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.NotNull;
 public class PerguntasModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "perguntas_id")
     private long id;
 
     @NotNull
